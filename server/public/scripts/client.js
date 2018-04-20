9229255d8e4e9616d80ccda88df84b1ba5ced882
Cory Booth <Rodimus2005@hotmail.com>
2018-04-19 21:45:04 -0500

    Did I just say game?

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,18 +3,59 @@ console.log('script.js is Sploaded')
 
 $(document).ready(onReady);
 
-function onReady() { 
+function onReady() {
     console.log('jQuery is go for launch');
-   $('#quotes').on('click', clickAjax);
+    $('#quotes').on('click', clickAjax);
+    $('#submitQuote').on('click', addQuote);
 
-   function clickAjax(){
     $.ajax({
         type: 'GET',
         url: '/quote'
     })
-    .then(function(response) {
-        $('#randoQuote').text(response.quote);
-        console.log(response);
-    });
-    }
+        .then(function (response) {
+            $('#randoQuote').text('"' + response.quote + '"' + ' -' + response.author);
+        });
+}
+
+function clickAjax() {
+    $.ajax({
+        type: 'GET',
+        url: '/quote'
+    })
+        .then(function (response) {
+            $('#randoQuote').text('"' + response.quote + '"' + ' -' + response.author);
+            console.log(response);
+        });
+
+}
+
+function addQuote() {
+    const newQuote = {
+        quote: $('#newQuote').val(),
+        author: $('#newAuthor').val(),
+    };
+    console.log('New quote object', newQuote);
+    $.ajax({
+        method: 'POST',
+        url: '/add-quote',
+        data: newQuote
+    })
+        .then(function (response) {
+            console.log(response);
+            getAllQuotes();
+        });
+}
+
+function getAllQuotes() {
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+        .then(function (response) {
+            response.forEach(function (element) {
+                $('#allQuotes').append('"' + element.quote + '"' + ' -' + element.author);
+            });
+
+
+        })
 }
--- server/public/server.js
+++ server/public/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 const quotes_data = require('./modules/quotes-data');
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/quote', (req, res) => {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -12,6 +14,12 @@ app.get('/quote', (req, res) => {
 
 app.get('/all-quotes', (req, res) => {
     res.send(quotes_data);
+});
+
+app.post('/add-quote', (req, res) => {
+    console.log(req.body);//needs body parser
+    quotes_data.push(req.body);
+    res.sendStatus(200);
 });
 
 app.get('/', (req, res) => {
